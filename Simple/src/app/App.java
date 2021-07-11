7b9c23c3367dbb47fbc7aa7175616643ca4889c8
Timur <pexaev@mail.ru>
2021-07-11 03:36:21 +0300

    A little change

--- Simple/src/app/App.java
+++ Simple/src/app/App.java
@@ -1,7 +1,8 @@
+// git init; git commit -m "new msg"; git add .; git push;
 package app;
 
 public class App {
     public static void main(String[] args){
-        System.out.println("Hello world!!!!");
+        System.out.println("Hello world!!");
     }
 }
