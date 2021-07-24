cae67051edbaf8779406cac3a8b59b9adf359108
Timur <pexaev@mail.ru>
2021-07-24 08:17:28 +0300

    Checking retrieved users

--- Donwnloading MySQL Connector/src/main/java/com/fromthecircus/db/User.java
+++ Donwnloading MySQL Connector/src/main/java/com/fromthecircus/db/User.java
@@ -1,6 +1,8 @@
 
 package com.fromthecircus.db;
 
+import java.util.Objects;
+
 public class User {
 
     private int m_id;
@@ -41,5 +43,18 @@ public class User {
                 "id=" + m_id +
                 ", name='" + m_name + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return m_id == user.m_id && m_name.equals(user.m_name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(m_id, m_name);
     }
 }
